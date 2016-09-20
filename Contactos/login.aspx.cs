672bed676116514d6afd04318cf3d65347e3516a
Jose Maximino Chavez <josemc.ortiz@gmail.com>
2016-09-20 04:55:14 -0400

    Muchos

---
+++ Contactos/login.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Contactos
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonBuscar_Click(object sender, EventArgs e)
+        {
+            //Almacenamos las dos variables de sesion
+            Session["usuario"] = TextBoxId.Text;
+            Session["clave"] = TextBoxName.Text;
+            //Redireccionamos a la siguiente pagina
+            Response.Redirect("Contactanos.aspx");
+        }
+    }
+}
